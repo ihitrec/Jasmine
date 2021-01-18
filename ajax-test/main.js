c86cc55d69fdd1b1e40eb87ae7f5991c7235dd23
ihitrec <hitrec555@gmail.com>
2021-01-18 00:48:52 +0000

    push

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,41 +1,52 @@
+
+// Enable input on table cells
 $("td").attr("contenteditable", "true");
+
+// Add a key up function on each cell
 $("td").attr("onkeyup", "myFunc()");
-let numKey;
-let whichCell;
+
+// If entered key is a number, keep it. Otherwise delete it.
 function myFunc() {
+    let whichCell = this.event.path[0];
     if (Number.isNaN(parseInt(this.event.key)) == false) {
-        numKey = this.event.key;
-        whichCell = this.event.path[0];
-        logIt();
+        let numKey = this.event.key;
+        whichCell.innerText = numKey;
+    } else if (Number.isNaN(parseInt(this.event.key)) == true) {
+        whichCell.innerText = "";
     }
 
 }
 
-function logIt() {
-    whichCell.innerText = numKey;
-    console.log(numKey);
-    console.log(whichCell);
-}
-
-let cellArray = document.getElementsByTagName("td");
-
+// Generate a random cell using the cellArray variable 
 function pickACell() { return Math.floor(Math.random() * 81); }
-let alreadyPickedCell = [];
+
+// Generate a random number from 1 to 9
 function pickANumber() { return Math.floor(Math.random() * 9 + 1); }
+
+// Generate a random template of 17 filled cells at the start
+let alreadyPickedCell = [];
+let cellArray = document.getElementsByTagName("td");
 function generateTemplate() {
+
     for (i = 0; i < 17; i++) {
         let pickedCell = pickACell();
+        /* If the cell picked in the current iteration doesn't match any that were previously picked,
+           add a random number to it. Otherwise skip it and reduce the count by one. */
         if (alreadyPickedCell.includes(pickedCell) === false) {
-            console.log(pickedCell);
             cellArray[pickedCell].innerText = pickANumber();
             alreadyPickedCell.push(pickedCell);
         } else if (alreadyPickedCell.includes(pickedCell) === true) {
             i--;
         }
-
-        console.log(alreadyPickedCell);
     }
-
-
 }
 generateTemplate();
+function resetTemplate() {
+    for (i = 0; i < cellArray.length; i++) {
+        cellArray[i].innerText = "";
+    }
+}
+$("button").click(function () {
+    resetTemplate();
+    generateTemplate();
+})
