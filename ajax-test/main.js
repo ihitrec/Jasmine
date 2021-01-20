bf216fec9e687cd6a7e933eba00399837580f137
ihitrec <hitrec555@gmail.com>
2021-01-20 21:33:22 +0000

    push

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,5 +1,5 @@
 
-// Enable input on table cells
+// Enable input on all table cells
 $("td").attr("contenteditable", "true");
 
 // Add a key up function on each cell
@@ -26,46 +26,90 @@ function pickANumber() { return Math.floor(Math.random() * 9 + 1); }
 // Generate a random template of 17 filled cells at the start
 let alreadyPickedCells = [];
 let cellArray = document.getElementsByTagName("td");
+let outsideCount = 0;
 function generateTemplate() {
-    for (i = 0; i < 17; i++) {
+    for (i = 0; i < 72; i++) {
         let pickedCell = pickACell();
+
+        // Colum check-------------------------------------------------------------------------------START
         // Get the index of the picked cell and reduce it to the first at the top of the column
         let reduceToSameColumn = pickedCell;
-        if (reduceToSameColumn > 9) {
+        if (reduceToSameColumn > 8) {
             let fitsHowMany = Math.floor(reduceToSameColumn / 9);
             reduceToSameColumn = reduceToSameColumn - (9 * fitsHowMany);
         }
         // Get all indexes of the cells in the same column
         let sameColumn = [];
+
         for (j = reduceToSameColumn; j < 81; j += 9) {
             sameColumn.push(j);
+
         }
+        let reduceToSameRow = sameColumn[0] // Belongs to row check
+
         // Remove the picked cell so it does not check its own innerText
         let removeACell = sameColumn.indexOf(pickedCell);
         sameColumn.splice(removeACell, 1);
+
+
+        // Colum check---------------------------------------------------------------------------------END
+
+        let firstRowIndex = pickedCell - reduceToSameRow;
+        let nineTimes = 0;
+        let sameRow = [];
+
+        for (nineTimes = 0; nineTimes < 9; nineTimes++) {
+            if (nineTimes === reduceToSameRow) {
+                continue;
+            } else {
+                sameRow.push(cellArray[firstRowIndex + nineTimes]);
+            }
+        }
         /* If the cell picked in the current iteration doesn't match any that were previously picked,
            add number between 1 and 9 to it. Otherwise skip it and reduce the count by one. */
         if (alreadyPickedCells.includes(pickedCell) === false) {
             cellArray[pickedCell].innerText = pickANumber();
             alreadyPickedCells.push(pickedCell);
-            // If the current cell matches the text of any in the same column, reset its text and reduce the  generateTemplate count by 1
-            for (k = 0; k < sameColumn.length; k++) {
-                if (cellArray[pickedCell].innerText === cellArray[sameColumn[k]].innerText) {
+            // If the current picked cell matches the text of any in the same column, reset its text and reduce the  generateTemplate count by 1
+            for (k = 0; k < 8; k++) {
+                if (cellArray[pickedCell].innerText === cellArray[sameColumn[k]].innerText || cellArray[pickedCell].innerText === sameRow[k].innerText) {
                     cellArray[pickedCell].innerText = "";
+                    outsideCount += 1;
                     i--;
-                } else if (cellArray[pickedCell].innerText === cellArray[sameRow[k]].innerText) {
+                    k = 20;
+                    alreadyPickedCells.pop();
 
                 }
             }
         } else if (alreadyPickedCells.includes(pickedCell) === true) {
             i--;
         }
+
+        if (outsideCount > 1000) {
+            console.log(outsideCount);
+            for (mile = 0; mile < 81; mile++) {
+                cellArray[mile].innerText = "";
+
+            }
+            outsideCount = 0;
+            i = 0;
+            alreadyPickedCells = [];
+
+        }
     }
     alreadyPickedCells = [];
 }
+// console.log(outsideCount);
+// if (outsideCount > 1000) {
+//     console.log(outsideCount);
+// }
 
 // Generate template when the page is refreshed
 generateTemplate();
+// setTimeout(function () {
+//     generateTemplate();
+// }, 10000)
+
 
 // Reset the text of each cell
 function resetTemplate() {
